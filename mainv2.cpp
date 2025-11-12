5e449c48a985907bfb7e53e9247e3830c84a019b
mrCodeScripts <kqlyshock@gmail.com>
2025-11-12 13:24:52 +0800

    Just finished shit

--- mainv2.cpp
+++ mainv2.cpp
@@ -8,7 +8,6 @@
 #include <utility>
 #include <conio.h>
 #include <string>
-#include <vector>
 
 void makeTopBottomEdgeBorder(int bxLen, std::string &rEdgeChar, std::string &lEdgeChar, std::string &midEdge, std::vector<std::pair<std::string, std::string>> &colors, std::string &frame)
 {
@@ -24,6 +23,7 @@ void makeTopBottomEdgeBorder(int bxLen, std::string &rEdgeChar, std::string &lEd
     }
     frame += '\n';
 }
+
 void makeTextWithColors(std::string &introPhrase, std::vector<std::pair<std::string, std::string>> &colors, int &xTabSize, int &yTabSize, std::string &midVertEdge, std::string &frame)
 {
     int index = 0;
@@ -31,23 +31,7 @@ void makeTextWithColors(std::string &introPhrase, std::vector<std::pair<std::str
     std::string topBottomPadding;
     std::string leftPadding;
     std::string rightPadding;
-    if (yTabSize > 0)
-    {
-        int xfullSpace = (xTabSize * 2) + introPhrase.size();
-        for (int j = 1; j <= yTabSize; j++)
-        {
-            // topBottomPadding += '\n';
-            for (int i = 1; i <= xfullSpace; i++)
-            {
-                if (i == 1)
-                    topBottomPadding += colors[0].first + midVertEdge + colors[0].second;
-                else if (i == xfullSpace)
-                    topBottomPadding += colors[0].first + midVertEdge + colors[0].second;
-                else
-                    topBottomPadding += " ";
-            }
-        }
-    }
+
     if (xTabSize > 0)
     {
         for (int i = 1; i <= xTabSize; i++)
@@ -65,6 +49,7 @@ void makeTextWithColors(std::string &introPhrase, std::vector<std::pair<std::str
                 rightPadding += ' ';
         }
     }
+
     for (const char &c : introPhrase)
     {
         textFrame += colors[index].first + std::string(1, c) + colors[index].second;
@@ -72,11 +57,10 @@ void makeTextWithColors(std::string &introPhrase, std::vector<std::pair<std::str
         if (index >= colors.size())
             index = 0;
     }
-    frame += topBottomPadding;
+
     frame += '\n' + leftPadding;
     frame += textFrame;
     frame += rightPadding + '\n';
-    frame += topBottomPadding;
 }
 
 void introduction(float &accumilator, float &dt, float &speed, std::string &frame, std::chrono::high_resolution_clock::time_point &lastFrame, std::vector<std::pair<std::string, std::string>> &colors)
@@ -98,7 +82,6 @@ void introduction(float &accumilator, float &dt, float &speed, std::string &fram
     std::string introPhrase = "Welcome to our restaurant";
     int introPhraseSize = introPhrase.size();
     int xTabSize = 5;
-    int yTabSize = 1;
     int bxLen = xTabSize * 2 + introPhraseSize;
 
     std::string lTopEdge = u8"╔";
@@ -109,12 +92,12 @@ void introduction(float &accumilator, float &dt, float &speed, std::string &fram
     std::string midHorEdge = u8"═";
 
     makeTopBottomEdgeBorder(bxLen, rTopEdge, lTopEdge, midHorEdge, colors, frame);
-    makeTextWithColors(introPhrase, colors, xTabSize, yTabSize, midVertEdge, frame);
+    makeTextWithColors(introPhrase, colors, xTabSize, xTabSize, midVertEdge, frame);
     makeTopBottomEdgeBorder(bxLen, rBottomEdge, lBottomEdge, midHorEdge, colors, frame);
     frame += '\n';
 }
 
-void detectKeyboard(int &index, int maxIndex, bool &pressEntered)
+void detectKeyboard(int &index, int maxIndex, bool &pressEntered, bool &pressEsc)
 {
     if (_kbhit())
     {
@@ -124,12 +107,12 @@ void detectKeyboard(int &index, int maxIndex, bool &pressEntered)
             k = _getch();
             switch (k)
             {
-            case 72:
+            case 72: // up
                 index--;
                 if (index < 0)
                     index = maxIndex;
                 break;
-            case 80:
+            case 80: // down
                 index++;
                 if (index > maxIndex)
                     index = 0;
@@ -137,14 +120,10 @@ void detectKeyboard(int &index, int maxIndex, bool &pressEntered)
             }
         }
         else if (k == 13)
-        {
             pressEntered = true;
-        }
+        else if (k == 27)
+            pressEsc = true;
     }
-}
-
-void setChosenFoods(std::vector<std::pair<int, double>> &chosenFoods)
-{
 }
 
 void properClear()
@@ -178,139 +157,132 @@ void chooseFood(std::vector<std::pair<double, std::string>> &foods, std::vector<
     int index = 0;
     bool initialClear = false;
     bool hasFinalyChosenFood = false;
-    std::vector<std::pair<int, int>> chosenFood; // foodIndex, foodQuantity
+    bool pressEsc = false;
 
     removeCursor();
+
     while (choosingFood)
     {
         if (initialClear)
-        {
             properClear();
-        }
         else
         {
             hardClear();
             initialClear = true;
         }
 
-        if (hasFinalyChosenFood) {
-
-        }
-
         std::string frame;
         introduction(accumilator, dt, speed, frame, lastFrame, colors);
+
+        frame += "Choose your food (ENTER to add, ESC to return)\n\n";
         for (int i = 0; i < foods.size(); i++)
         {
             if (i == index)
-            {
-                frame += u8"►" + std::string("\033[1;92m") + " $" + std::to_string(foods[i].first) + " " + foods[i].second + "\033[0m" + '\n';
-            }
+                frame += u8"► \033[1;92m$" + std::to_string(foods[i].first) + " " + foods[i].second + "\033[0m\n";
             else
-            {
-                frame += "  $" + std::to_string(foods[i].first) + " " + foods[i].second + '\n';
-            }
+                frame += "  $" + std::to_string(foods[i].first) + " " + foods[i].second + "\n";
         }
-        std::cout << frame;
 
-        detectKeyboard(index, foods.size() - 1, hasFinalyChosenFood);
+        std::cout << frame;
+        detectKeyboard(index, foods.size() - 1, hasFinalyChosenFood, pressEsc);
+
+        if (hasFinalyChosenFood)
+        {
+            chosenFoods.push_back({index, foods[index].first});
+            hasFinalyChosenFood = false;
+        }
+        if (pressEsc)
+            choosingFood = false;
     }
 }
 
-
-
-void menuSelection (std::vector<std::pair<std::string, std::vector<std::pair<double, std::string>>>> &restaurantMenu, float &accumilator, float &dt, float &speed, std::chrono::high_resolution_clock::time_point &lastFrame, std::vector<std::pair<std::string, std::string>> &colors) {
+void menuSelection(std::vector<std::pair<std::string, std::vector<std::pair<double, std::string>>>> &restaurantMenu, std::vector<std::pair<std::string, int>> &chosenFoods, float &accumilator, float &dt, float &speed, std::chrono::high_resolution_clock::time_point &lastFrame, std::vector<std::pair<std::string, std::string>> &colors)
+{
     bool initialClear = false;
     bool choosing = true;
-    bool choosingMenuIndex = true;
-    bool choosingFoodIndex = false;
-    bool hasFinallyChossenMenu = false;
+    bool hasFinallyChosenMenu = false;
+    bool pressEsc = false;
     int menuIndex = 0;
-    int foodIndex = 0; 
 
-    std::vector<std::pair<int, double>> chosenFoods; // pair(id, price)
+    std::vector<std::pair<int, double>> chosenFoods;
 
     removeCursor();
 
-    while (choosing) {
-        if (initialClear) {
+    while (choosing)
+    {
+        if (initialClear)
             properClear();
-        } else {
+        else
+        {
             hardClear();
             initialClear = true;
         }
 
         std::string frame;
-        if (choosingMenuIndex && !(choosingFoodIndex) && !(hasFinallyChossenMenu)) {
-            for (int i = 0; i < restaurantMenu.size(); i++) {
-                if (i == menuIndex) {
-                    frame += "> " + restaurantMenu[i].first + '\n';
-                } else {
-                    frame += " " + restaurantMenu[i].first + '\n';
-                }
-            }
-        } else {
-            std::vector<std::pair<double, std::string>> availableFoods;
-            chooseFood(availableFoods, chosenFoods, accumilator, dt, speed, lastFrame, colors);
+        introduction(accumilator, dt, speed, frame, lastFrame, colors);
+        frame += "Select Menu (ENTER to open)\n\n";
+
+        for (int i = 0; i < restaurantMenu.size(); i++)
+        {
+            if (i == menuIndex)
+                frame += u8"► \033[1;93m" + restaurantMenu[i].first + "\033[0m\n";
+            else
+                frame += "  " + restaurantMenu[i].first + "\n";
         }
+
         std::cout << frame;
 
-        detectKeyboard(menuIndex, restaurantMenu.size() - 1, hasFinallyChossenMenu);
+        detectKeyboard(menuIndex, restaurantMenu.size() - 1, hasFinallyChosenMenu, pressEsc);
+
+        if (hasFinallyChosenMenu)
+        {
+            chooseFood(restaurantMenu[menuIndex].second, chosenFoods, accumilator, dt, speed, lastFrame, colors);
+            hasFinallyChosenMenu = false;
+        }
+
+        if (pressEsc)
+            choosing = false;
     }
+
+    hardClear();
+    double total = 0;
+    std::cout << "\n\033[1;94mYour Chosen Foods:\033[0m\n";
+    for (auto &cf : chosenFoods)
+    {
+        total += cf.second;
+        std::cout << "- $" << cf.second << "\n";
+    }
+    std::cout << "\n\033[1;92mTotal: $" << total << "\033[0m\n\n";
+    std::cout << "Press any key to exit...";
+    _getch();
 }
-
-
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    float accumilator; // accumilator = speed * dt
-    float dt;
+    float accumilator = 0;
+    float dt = 0;
     float speed = 0.9f;
     auto lastFrame = std::chrono::high_resolution_clock::now();
     std::vector<std::pair<std::string, std::string>> colors = {
-        {"\033[1;91m", "\033[0m"}, // Bright Red
-        {"\033[1;92m", "\033[0m"}, // Bright Green
-        {"\033[1;93m", "\033[0m"}, // Bright Yellow
-        {"\033[1;94m", "\033[0m"}, // Bright Blue
-        {"\033[1;95m", "\033[0m"}, // Bright Magenta
-        {"\033[1;96m", "\033[0m"}, // Bright Cyan
-        {"\033[1;97m", "\033[0m"}  // Bright White
-    };
-
-    std::vector<std::pair<double, std::string>> foods = {
-        {99.99, "Burger King"},
-        {99.99, "Burger King"},
-        {99.99, "Burger King"},
-        {99.99, "Burger King"},
-        {99.99, "Burger King"},
-        {99.99, "Burger King"},
-    };
+        {"\033[1;91m", "\033[0m"},
+        {"\033[1;92m", "\033[0m"},
+        {"\033[1;93m", "\033[0m"},
+        {"\033[1;94m", "\033[0m"},
+        {"\033[1;95m", "\033[0m"},
+        {"\033[1;96m", "\033[0m"},
+        {"\033[1;97m", "\033[0m"}};
+    
+    std::vector<std::pair<std::string, int>> chosenFoods; // {foodId, foodQuantity}
 
     std::vector<std::pair<std::string, std::vector<std::pair<double, std::string>>>> restaurantMenu = {
-        {"Menu 1", {
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" }
-        }},
-        {"Menu 2", {
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" }
-        }},
-        {"Menu 3", {
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" },
-            { 99.99, "Burger King" }
-        }},
-    };
+        {"🍔 Food", {{99.99, "Classic Burger"}, {129.50, "Cheese Burger"}, {149.75, "Bacon Burger"}}},
+        {"🍧 Desert", {{199.99, "Pepperoni Pizza"}, {249.99, "Four Cheese"}, {229.99, "Hawaiian"}}},
+        {"🥤 Drinks", {{49.99, "Iced Tea"}, {59.99, "Coke"}, {39.99, "Water"}}}};
 
-    menuSelection(restaurantMenu, accumilator, dt, speed, lastFrame, colors);
+    menuSelection(restaurantMenu, chosenFoods, accumilator, dt, speed, lastFrame, colors);
 
     return 0;
 }
